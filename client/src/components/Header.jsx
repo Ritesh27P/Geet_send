b4b0862ea10cf92b50f4b74c8b0c2df28612e5a1
unknown <ankitprajapti1300@gmail.com>
2024-02-11 17:57:47 +0530

    User side front

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 import { Logo } from "../assets/img/index"
 import { NavLink } from 'react-router-dom'
 import {isActiveStyles, isNotActiveStyles} from "../utils/styles"
+
+import {FaCrown} from "react-icons/fa"
+
+
+
+
+
 function Header() {
     return (
         <header className="flex items-center w-full p-4 md:py-2 md:px-6">
@@ -16,6 +23,15 @@ function Header() {
                 <li className="mx-5 text-lg"><NavLink to={"/primium"} className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles}>Primium</NavLink></li>
                 <li className="mx-5 text-lg"><NavLink to={"/contact"} className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles}>Contact Us</NavLink></li>
             </ul>
+
+
+            <div className="flex items-center ml-auto cursor-pointer gap-2 relative">
+                <img src="" className="w-12 min-w-[44px] object-cover rounded-full shadow-lg" alt="" />
+                <div className="flex flex-col">
+                    <p className=" text-textColor text-lg hover:text-headingColor font-semibold">UserName</p>
+                    <p className=" flex items-center gap-2 text-xs text-green-500">Premium Member. <FaCrown className="text-sm -ml-1 text-yellow-500"></FaCrown></p>
+                </div>
+            </div>
 
         </header>
 
