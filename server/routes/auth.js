789ca5b3cbdfda7a87a46b8286bd9ef5aa9a403f
unknown <ankitprajapti1300@gmail.com>
2024-02-11 16:29:22 +0530

    Auth updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
+const user = require("../models/user")
+
 const admin = require("../config/firebase.config")
 router.get("/login", async (req, res) => {
     if(!req.headers.authorization){
@@ -10,12 +12,19 @@ router.get("/login", async (req, res) => {
     try{
         const decodeValue = await admin.auth().verifyIdToken(token);
         if(!decodeValue){
-            return res.status(505).send({message : "Un Authorized"});
+            return res.status(505).json({message : "Un Authorized"});
         }else{
-            return res.status(200).send(decodeValue);
+            // Checking User exists or not
+            // return res.status(200).json(decodeValue);
+            const userExists = await user.findOne({"user_id": decodeValue.user_id})
+            if(!userExists){
+                return res.send("Need to Create")
+            }else{
+                return res.send("Need to update user")
+            }
         }
     }catch(error){
-        return res.status(505).send({message : error});
+        return res.status(505).json({message : error});
     }
 })
 
