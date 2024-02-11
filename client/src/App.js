80c2ea5443eaea078503b2228d66682acdd3596e
unknown <ankitprajapti1300@gmail.com>
2024-02-11 21:24:52 +0530

    saved

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,14 @@ import { getAuth } from "firebase/auth"
 
 import { AnimatePresence } from "framer-motion"
 import {validateUser} from "./api"
-
+// import { useStateValue } from "./context/StateProvider";import { actionType } from "./context/reducer";
 
 function App() {
 
   const firebaseAuth = getAuth(app);
   const navigate = useNavigate();
 
+  // const [{user}, dispatch] = useStateValue();
 
 
   const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true")
@@ -26,6 +27,11 @@ function App() {
           // console.log(token);
           validateUser(token).then((data) => {
             console.log(data)
+            // dispatch({
+            //   type: actionType.SET_USER,
+            //   user : data
+            // })
+
           })
         })
       } else {
@@ -36,7 +42,7 @@ function App() {
     })
   }, []);
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence >
       <div className=" h-auto min-w-[680px] bg-primary justify-center items-center">
         <Routes>
           <Route path="/login" element={<Login setAuth={setAuth} />}></Route>
