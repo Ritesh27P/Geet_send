15fbf3e19655f7a69a0c3917df9d116ed261bec9
unknown <ankitprajapti1300@gmail.com>
2024-02-11 15:23:39 +0530

    Successful Authorized the decodeValue

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
+const admin = require("../config/firebase.config")
 
-router.get("/login", (req, res) => {
+router.get("/login", async (req, res) => {
     if(!req.headers.authorization){
         return res.status(500).send({message : "Invalid Token"});
     }
 
     const token = req.headers.authorization.split(" ")[1];
-    return res.send(token)
+    try{
+        const decodeValue = await admin.auth().verifyIdToken(token);
+        if(!decodeValue){
+            return res.status(500).send({message : "Un Authorized"});
+        }else{
+            return res.status(200).send(decodeValue);
+        }
+    }catch(error){
+        return res.status(500).send({message : error});
+    }
 })
 
 module.exports = router;
