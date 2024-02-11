59288cf204c38c176004d1435a95924b2eaa9096
unknown <ankitprajapti1300@gmail.com>
2024-02-11 16:41:44 +0530

    Auth updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -18,7 +18,8 @@ router.get("/login", async (req, res) => {
             // return res.status(200).json(decodeValue);
             const userExists = await user.findOne({"user_id": decodeValue.user_id})
             if(!userExists){
-                return res.send("Need to Create")
+                // return res.send("Need to Create")
+                newUserData(decodeValue, req, res)
             }else{
                 return res.send("Need to update user")
             }
@@ -27,5 +28,24 @@ router.get("/login", async (req, res) => {
         return res.status(505).json({message : error});
     }
 })
+
+const newUserData = async (decodeValue, req, res) => {
+    const newUser = new user({
+        name : decodeValue.name,
+        email : decodeValue.email,
+        imageURL : decodeValue.picture,
+        user_id : decodeValue.user_id,
+        email_verified : decodeValue.email_verified,
+        role : "member",
+        auth_time : decodeValue.auth_time
+    })
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(200).send({user: savedUser})
+    }catch (error){
+        res.status(400).send({success : false, msg : error});
+    }
+}
 
 module.exports = router;
